80016fc06a0659c84a48a65ec59bc33b8ac39fa6
Rakshith Ballal <rakshithballal@Rakshiths-MacBook-Pro.local>
2019-04-09 16:14:34 -0400

    Added CentOS package

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,10 +3,12 @@ class apache::params  {
 $service_name = $facts['os']['name'] ? {
   'Debian' => 'apache',
   'Redhat' => 'httpd',
+  'CentOs' => 'httpd',
   }
 
 $package_name = $facts['os']['name'] ? {
     'Debian' => 'apache',
     'Redhat' => 'httpd',
+    'CentOS' => 'httpd',
   }
 }
--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -4,6 +4,21 @@ describe 'apache::config' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      it { is_expected.to contain_file('/etc/httpd/conf/httpd.conf')
+        .with(
+        ensure: 'file',
+        )
+      }
+      it { is_expected.to contain_file('/var/www')
+        .with(
+        ensure: 'directory',
+        )
+      }
+      it { is_expected.to contain_file('/var/www/index.html')
+        .with(
+        ensure: 'file',
+        )
+      }
 
       it { is_expected.to compile }
     end
