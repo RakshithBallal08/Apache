f711f38a7fc8fe01beb7104bd63ffc9eaa2f934e
Rakshith Ballal <rakshithballal@Rakshiths-MacBook-Pro.local>
2019-04-09 15:06:25 -0400

    Adding apache module

--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -4,6 +4,12 @@ describe 'apache::install' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      let(:params) { { 'package_name' => 'httpd' } }
+      it { is_expected.to contain_package('httpd')
+        .with(
+        ensure: 'installed',
+        )
+      }
 
       it { is_expected.to compile }
     end
--- spec/classes/service_spec.rb
+++ spec/classes/service_spec.rb
@@ -4,6 +4,12 @@ describe 'apache::service' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      let(:params) { { 'service_name' => 'httpd' } }
+      it { is_expected.to contain_service('httpd')
+        .with(
+        ensure: 'running',
+        )
+      }
 
       it { is_expected.to compile }
     end
